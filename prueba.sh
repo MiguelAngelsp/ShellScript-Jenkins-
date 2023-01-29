1c3cbeeebce477d5db0019af1bf57d158cfe66c9
Miguel Angel <ma@vmi1170525.contaboserver.net>
2023-01-29 18:37:25 +0100

    Prueba_Hook_29-01-2023

--- prueba.sh
+++ prueba.sh
@@ -2,7 +2,7 @@
 echo "Ejecucion de prueba ssh despues de hacer un push local..."
 echo "Segunda prueebaa..."
 echo "jenkins, jenkins..."
-for i in 1 2 3 4 5
+for i in 1 2 3 4 5 6 7 8 9
 do
 	echo "Bienvenido $i veces"
 done
